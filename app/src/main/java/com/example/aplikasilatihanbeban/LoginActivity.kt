efd6940f0dc04517b093ecb5d35dbe57335fc327
akmalmaulanak <akmalkismoyo@gmail.com>
2024-11-04 00:12:19 +0700

    create repo

---
+++ app/src/main/java/com/example/aplikasilatihanbeban/LoginActivity.kt
@@ -0,0 +1,24 @@
+package com.example.aplikasilatihanbeban
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.aplikasilatihanbeban.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+}
---
+++ app/src/main/java/com/example/aplikasilatihanbeban/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.aplikasilatihanbeban
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val buttonNext: Button = findViewById(R.id.button)
+
+        buttonNext.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
